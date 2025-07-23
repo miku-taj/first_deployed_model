5973af204fb5acc6385954ab5ec0e2dedeb03d28
miku-taj <115816104+miku-taj@users.noreply.github.com>
2025-07-23 18:55:23 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 
-st.set_page_config(page_title="Penguin Classifier", layout="wide")
+st.set_page_config(page_title="🐧 Penguin Classifier", layout="wide")
 st.title('Penguin Classifier')
 st.write('Working with penguin dataset')
 
 df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+st.subheader("Random 10 rows")
+st.dataframe(df.sample(10), use_container_width=True)
